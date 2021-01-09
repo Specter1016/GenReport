95f9bac10f9c97445755bed3a35402bf8262a7ed
Specter1016 <base_1996@hotmail.co.th>
2021-01-09 22:02:05 +0700

    edite install

--- install.bat
+++ install.bat
@@ -1,7 +1,6 @@
-cd Python\Scripts
-pip install --upgrade pip
-pip install bs4
-pip install lxml
-pip install requests
-pip install art
-pip install selenium
+.\Python\python pip install --upgrade pip
+.\Python\python pip install bs4
+.\Python\python pip install lxml
+.\Python\python pip install requests
+.\Python\python pip install art
+.\Python\python pip install selenium
