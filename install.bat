11ed35aeae76b17cc9610b6319b65ed4fadf75ce
Specter1016 <base_1996@hotmail.co.th>
2021-01-09 22:07:49 +0700

    2 install

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
-.\Python\python Scripts\pip install --upgrade pip
-.\Python\python Scripts\pip install bs4
-.\Python\python Scripts\pip install lxml
-.\Python\python Scripts\pip install requests
-.\Python\python Scripts\pip install art
-.\Python\python Scripts\pip install selenium
+.\Python\python .\Python\Scripts\pip install --upgrade pip
+.\Python\python .\Python\Scripts\pip install bs4
+.\Python\python .\Python\Scripts\pip install lxml
+.\Python\python .\Python\Scripts\pip install requests
+.\Python\python .\Python\Scripts\pip install art
+.\Python\python .\Python\Scripts\pip install selenium
