3baba44b6d2c502999a3e8c17082ee494a49fab9
Specter1016 <basae_1996@hotmail.co.th>
2021-02-19 23:52:38 +0700

    new path 02-19-2021

--- MonitorReport.py
+++ MonitorReport.py
@@ -284,7 +284,7 @@ def setSendReport():
 
         #setDate
 
-        setd = str(time.strftime("%m/%d/%Y"));
+        setd = str(time.strftime("%d/%m/%Y"));
         print(setd);
         time.sleep(5)
         setdate = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/div/input[1]');
@@ -497,12 +497,91 @@ def notifyLine():
 
 
 
-readHTML();
-readset();
+def newReadVersion():
+
+    with open('DataFile/cpu/Dashboards - vRealize Operations Manager.html','r',encoding="utf-8") as read_cpu:
+
+        with open('ValueReport.text','w') as setNewData:
+
+            with open('Royalcpu.text', 'w') as setNewcpu:
+
+                with open('Royalram.text', 'w') as setNewram:
+
+                    with open('Royalnet.text', 'w') as setNewnet:
+
+                        get_cpu = read_cpu.readlines();
+
+
+                        count=0;
+                        setsettt = 0;
+                        get_cpu_step3 = "";
+                        countstate = 0;
+
+                        opencpu = [];
+                        openram = [];
+                        opennet = [];
+
+                        for x in get_cpu:
+
+                            if '<b>H:</b> ' in x:
+
+                                    get_cpu_step2 = x.split('<b>H:</b>');
+                                    print(len(get_cpu_step2));
+
+                                    for y in get_cpu_step2:
+
+
+                                        if setsettt == 0:
+
+                                            setsettt = setsettt+1;
+
+                                        else:
+
+                                            for z in y :
+
+                                                if z != '<' and count == 0:
+
+                                                    count =count +1;
+
+                                                if z !=[] and z != '<' and count !=0:
+
+                                                    get_cpu_step3 = get_cpu_step3+str(z);
+
+
+                                                else:
+
+                                                    if countstate <= 10:
+
+                                                        setNewcpu.writelines(format(get_cpu_step3).strip());
+                                                        setNewcpu.writelines('\n');
+                                                        get_cpu_step3="";
+                                                        countstate = countstate+1;
+                                                        break;
+
+                                                    if countstate <= 21:
+
+                                                        setNewram.writelines(format(get_cpu_step3).strip());
+                                                        setNewram.writelines('\n');
+                                                        get_cpu_step3 = "";
+                                                        countstate = countstate + 1;
+                                                        break;
+
+                                                    if countstate <= 32:
+                                                        setNewnet.writelines(format(get_cpu_step3).strip());
+                                                        setNewnet.writelines('\n');
+                                                        get_cpu_step3 = "";
+                                                        countstate = countstate + 1;
+                                                        break;
+
+
+#readHTML();
+#readset();
 
 #setCPUFuntion();
 #setRAMFuntion();
 #setNETFuntion();
+
+newReadVersion();
 setShowReport();
 setReportFile();
 setSendReport();
